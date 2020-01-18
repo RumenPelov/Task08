c462f38b95b0a7f7568522693ded1cb924b8a27f
Rumen Pelov <rumenpelov@yahoo.com>
2020-01-18 05:24:35 -0500

    Message component

--- src/components/Message.js
+++ src/components/Message.js
@@ -1,20 +1,24 @@
 import React from 'react';
+import moment from 'moment'
 
-const Massage = ({msg}) => {
-    const date = new Date(msg.created_at);
-    const date_created = date.toLocaleDateString("en-GB", {month:'short', day:'numeric'})
+const Massage = ({msg:{msg, count, index}}) => {
+    const date = moment(msg.created_at).format("MMM Do");  
+    const score = msg.score || 1; 
 
     return (
         <div className="message">
             <div className="message__subject">
-                <h4> {msg.subject} </h4>
+                <h4 className={score >= 6 ? "high-rating" : "low-rating"}> {msg.subject} </h4>
                 <p className="paragraph-small mb-small mt-small">{msg.question}</p>
                 <p className="paragraph">{msg.text}</p>
                 
             </div>
             <div className="message__team">
                 <p className="paragraph-medium">{msg.team}</p>
-                <p className="paragraph-small mt-small">{date_created}</p>
+                <p className="paragraph-small mt-small">{date}</p>
+                <div className={count > 1 && index === 0 ? 
+                                score >= 6 ? "msg-counter bg-high-rating" : "msg-counter bg-low-rating" 
+                                : "hidden" }>{`${count} messages`}</div>
             </div>
         </div>
     
--- src/components/Thread.js
+++ src/components/Thread.js
@@ -2,9 +2,37 @@ import React from 'react';
 import Message from './Message';
 
 const Thread = ({thread}) => {
+    const sortedThread = thread.map(msg =>  msg.score ? {...msg} : {...msg, score:1})
+                            .sort((a, b) => b.score - a.score);
+    const collapsed = sortedThread.length > 1 ? true : false;
+
+    const renderThreads = () => {
+        return sortedThread.map((msg, index, arr) => {
+            const count = collapsed ? arr.length : 1;
+
+            const offset = collapsed ? {
+                top: `${(index)*3}px`,
+                left: `${(index)*3}px`,
+                zIndex: `${99-index}`
+            } : {};
+            
+            return (
+                <div key={index} 
+                     style={ offset } 
+                     className={index === 0 ? "expanded" :
+                     collapsed ? "collapsed" : "expanded"} >
+                    <Message msg={{msg, count, index}} /> 
+                </div>
+            )
+
+            
+        })
+    } 
 
     return (
-        <Message msg={thread[0]}  />
+        <div className="thred">
+            {renderThreads()}
+        </div>
     )
 }
 
